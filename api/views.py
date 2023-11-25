d1592878a4997b220356ac3e1a557b73c4749f2f
Humoyun <h.ahmedov209@gmail.com>
2023-11-25 10:16:13 +0500

    Refactoring

--- api/views.py
+++ api/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 
-from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
